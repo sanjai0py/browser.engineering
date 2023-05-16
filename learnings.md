d8a8b1a54e68aa0bb59f90eaac4dcecf1ef075fa
Sanjai Kumar <84461672+sanjai0py@users.noreply.github.com>
2023-05-16 16:15:50 +0530

    implemented request function

---
+++ learnings.md
@@ -0,0 +1,32 @@
+# Sockets API
+
+There are 2 types of **sockets**
+
+1. Network socket
+2. Unix Domain sockets (used to let programes running on a computer communicate)
+
+## socket.socket()
+
+this function creates an endpoint for communication it accepts **3** arguments **Domain, Type, Protocol**
+
+The socket family - Domain
+Determines whether you want a socket that works over the Internet or a local one. More specifically, you can have IPv4-based sockets, IPv6-based sockets or UNIX sockets.
+
+A socket configured as IPv4 or IPv6 can exchange data with remote hosts. The former works with IP addresses version 4, the latter works with IP addresses version 6. A socket configured as UNIX is used to exchange data between programs on the same machine. Windows, a non-UNIX operating system, recently added support for the UNIX socket type.
+
+The socket type - Type
+Determines the type of communication you want to establish. Here the choice boils down to three types: stream sockets for connection-oriented protocols such as the Transmission Control Protocol (TCP); datagram sockets for connectionless protocols such as the User Datagram Protocol; raw sockets for low-level communication protocols such as the Internet Protocol.
+
+With stream or datagram sockets you are using popular protocols already implemented for you by the operating system. Raw sockets instead allow to do whatever you want: you can implement your own protocols, generate custom IP packets, intercept network traffic or just mess around by sending invalid data to other computers.
+
+Raw sockets are powerful and might cause harm if used for malicious purposes. This is probably the reason why raw sockets on Windows are in read-only mode: you can't send data with them.
+
+The protocol - Protocol
+This piece of information is often optional, as sockets can automatically determine how to behave given the family and the type described above. For example a stream, IPv6-based socket is automatically prepared for a TCP-over-IPv6 transmission. Even better, the Berkeley sockets API includes some utility functions to determine the right parameters for the socket configuration given the address you want to connect to.
+
+## Flow chart
+
+[url](https://raw.githubusercontent.com/monocasual/internalpointers-files/master/2021/09/network6/client-server-calls.png)
+
+## HTTP response formate
+[url](http://www.tcpipguide.com/free/diagrams/httpresponse.png)
--- script.py
+++ script.py
@@ -1,50 +1,56 @@
 from urllib.parse import urlparse
 import socket
 
-# the url to request response for.
 url = "https://browser.engineering/index.html"
-# url = "http://example.org/index.html"
 
-# parsing the url
-host = urlparse(url)
+def request(url):
+    # Parse a URL into 6 components:
+    # <scheme>://<netloc>/< path >
+    # <params >?<query >#<fragment>
+    host = urlparse(url)
+
+# 1. Creating new socket
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    )
+
+# 2. Connecting to the socket
+    s.connect((host.netloc, 80))
+
+# 3. Sending requets for data
+    s.send(f"GET {host.path} HTTP/1.0\r\n".encode("utf8") +
+           f"Host: {host.netloc}\r\n\r\n".encode("utf8"))
+
+# 4. The response received from the server
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+    # reading the first line of the file
+    statusline = response.readline()
+
+    # HTTP/1.1 404 Not Found
+    version, status, explanation = statusline.split(" ", 2)
+
+    # Checking if the response is OK
+    assert status == "200", f"{status}: {explanation}"
+
+    # refer this image for better understanding
+    # http://www.tcpipguide.com/free/diagrams/httpresponse.png
+    headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n":
+            break
+        header, value = line.split(":", 1)
+        headers[header.lower()] = value.strip()
+
+    assert "transfer-encoding" not in headers
+    assert "content-encoding" not in headers
+
+    body = response.read()
+    s.close()
+    return body
 
 
-s = socket.socket(
-    family=socket.AF_INET,
-    type=socket.SOCK_STREAM,
-    proto=socket.IPPROTO_TCP,
-)
-
-s.connect((host.netloc, 80))
-
-s.send("GET {} HTTP/1.0\r\n".format(host.path).encode("utf8") +
-       "Host: {}\r\n\r\n".format(host.netloc).encode("utf8"))
-       
-response = s.makefile("r", encoding="utf8", newline="\r\n")
-
-statusline = response.readline()
-version, status, explanation = statusline.split(" ", 2)
-assert status == "200", "{}: {}".format(status, explanation)
-
-
-headers = {}
-while True:
-    line = response.readline()
-    if line == "\r\n":
-        break
-    header, value = line.split(":", 1)
-    headers[header.lower()] = value.strip()
-# print(headers)
-assert "transfer-encoding" not in headers
-assert "content-encoding" not in headers
-
-body = response.read()
-s.close()
-print(body)
-print(response.read())
-
-
-# creating a requets function that abstracts the above code
-
-def request (url):
-    print("Suuuuuuuuuuuuu")
+print(request(url))
